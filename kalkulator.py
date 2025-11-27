e1d546d24c334328c3a03d438b13b74e51a02efc
Jihan Humaira lenovo <lalahumaira00@gmail.com>
2025-11-27 07:30:55 +0700

    Update kalkulator1

--- kalkulator.py
+++ kalkulator.py
@@ -1,19 +1,11 @@
-# This function adds two numbers
 def add(x, y):
     return x + y
-
-# This function subtracts two numbers
 def subtract(x, y):
     return x - y
-
-# This function multiplies two numbers
 def multiply(x, y):
     return x * y
-
-# This function divides two numbers
 def divide(x, y):
     return x / y
-
 
 print("Select operation.")
 print("1.Add")
@@ -23,8 +15,6 @@ print("4.Divide")
 
 while True:
     choice = input("Enter choice(1/2/3/4): ")
-
-    # check if choice is one of the four options
     if choice in ('1', '2', '3', '4'):
         try:
             num1 = float(input("Enter first number: "))
@@ -32,16 +22,12 @@ while True:
         except ValueError:
             print("Invalid input. Please enter a number.")
             continue
-
         if choice == '1':
             print(add(num1, num2))
-
         elif choice == '2':
             print(subtract(num1, num2))
-
         elif choice == '3':
             print(multiply(num1, num2))
-
         elif choice == '4':
             print(divide(num1, num2))
     
